9234b86c84989a48268a803df13350ce28cdb078
Elizeu Freitas <elizeuferreiradefreitas@gmail.com>
2023-07-23 20:21:04 -0300

    Create Models

--- system/models.py
+++ system/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Produtos(models.Model):
+    Nome = models.CharField(max_length=65)
+    cód = models.IntegerField()
+    SKU = models.CharField(max_length=65)
+    Vendas = models.IntegerField()
+    EstoqueTotal = models.IntegerField()
+    CustoTotal = models.DecimalField(decimal_places=2, max_digits=9)
+    RevendaTotal = models.DecimalField(decimal_places=2, max_digits=9)
+    
