1141a8f70301ff85fc34f4cf5b7aa15a48b38a33
Elizeu Freitas <elizeuferreiradefreitas@gmail.com>
2023-08-08 14:10:38 -0300

    CREATE BLOCKS LOGIN

--- login/views.py
+++ login/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home_login(request):
-    return render(request,'login/login.html')
+    return render(request,'login/pages/login.html')
 
 # Create your views here.
